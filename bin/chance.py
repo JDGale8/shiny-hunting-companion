2f353558c1ecc1ff66b47ae2230fa8ba3036944a
Jake <Jake@JakeGale.com>
2016-11-27 20:49:12 +0000

    adds DexNav to cumulative chance calculator

--- bin/chance.py
+++ bin/chance.py
@@ -1,7 +1,5 @@
-
-
 def cum_bin_choice(c, n, m=1):
-    """ given a chance c, and number of trials n, returns chance a single success should have occured"""
+    """ given a chance c, and number of trials n, returns chance a single success should have occurred"""
     return 1 - (1 * c ** (m-1) * ((1 - c)**n))
 
 
@@ -65,10 +63,13 @@ def fishing(n, successes=1, shiny_charm=False, generation=7):
 
 
 def SOS(n, successes=1, shiny_charm=False, generation=7):
-    """ this is assuming SOS works in the same way as fishing"""
+    """
+    this is assuming SOS works in the same way as fishing
+    this is yet to be confirmed
+    """
     chain = 20 if n > 20 else n
 
-    base = 1 + 2*shiny_charm + 2 * chain
+    base = 1 + 2*shiny_charm + 2*chain
 
     if generation >= 6:
         chance = (1 - ((4095 / 4096) ** base))
@@ -80,20 +81,24 @@ def SOS(n, successes=1, shiny_charm=False, generation=7):
     return cum_chance
 
 
-def dex_nav(n, search_level, successes=1, shiny_charm=False):
-    search_level
+def dex_nav(n, successes=1, shiny_charm=False):
+    """
+    the jury seems a little out depending on the source you look at as to how the shiny chance
+    is affected by chaining the DexNav and by the shiny charm.
+    according to https://mrnbayoh.github.io/pkmn6gen/shiny_calculator/:
+        the search level and chain make a difference, however, I haven't found any other source to back this up
 
-    if n >= 50:
-        chain = 5
-    elif n >= 100:
-        chain = 10
-    else:
-        chain = 0
+    according to https://daily.pokecommunity.com/2016/06/13/all-about-shiny-pokemon/ and a similar reddit thread:
+        the chance increase (regardless of chain and search level) to something around 1/308
 
+    I don't know which one to trust at the moment, so let's just use the simpler version until more evidence is found
 
-    base = 1 + chain + 2*shiny_charm + random_bonus
+    """
 
-    pass
+    chance = 1/308
+    cum_chance = cum_bin_choice(chance, n, successes)
+
+    return cum_chance
 
 
 def horde(n, successes=1, shiny_charm=False, generation=7):
--- bin/shiny_sim.py
+++ bin/shiny_sim.py
@@ -1,4 +1,5 @@
 import random
+import chance as ch
 
 
 class Simulate:
