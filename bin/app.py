055d34766e89a7a9161193cd3283e3456d86bd34
Jake <Jake@JakeGale.com>
2017-01-02 11:41:23 -0800

    fixes typo into buton declaration

--- bin/app.py
+++ bin/app.py
@@ -14,7 +14,7 @@ from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QTextEdit
 from PyQt5.QtWidgets import QWidget
-import chance as ch
+import bin.chance as ch
 
 
 class TestGUI(QWidget):
@@ -22,7 +22,9 @@ class TestGUI(QWidget):
         super().__init__()
 
         self.fishing_counter = 0  # in the future - have this load from a json file
-        self.chance = 0
+        self.fishing_chance = 0
+        self.RE_counter = 0
+        self.RE_chance = 0
         self.initUI()
 
     def initUI(self):
@@ -36,15 +38,24 @@ class TestGUI(QWidget):
         self.counter_disp = QTextEdit(str(self.fishing_counter))
         self.counter_disp.textChanged.connect(self.set_chain)  # if it changes, change chain too
 
-        # set titles and info
+        # set titles and info for chain fishing
         self.fishing_title = QLabel("Chain Fishing")
         self.counter_label = QLabel("counter")
-        self.chance_lb = QLabel("Chance to have found a shiny by now:")
-        self.chance_amount = QTextEdit("{:.2f}".format(self.chance))
-        self.chance_amount.setReadOnly(True)
+        self.fishing_chance_lb = QLabel("Chance to have found a shiny by now:")
+        self.fishing_chance_amount = QTextEdit("{:.2f}".format(self.fishing_chance))
+        self.fishing_chance_amount.setReadOnly(True)
 
-        self.inc_btn = QPushButton("+")
-        self.inc_btn.clicked.connect(self.increase_fishing_counter)
+        self.inc_btn_fish = QPushButton("+")
+        self.inc_btn_fish.clicked.connect(self.increase_fishing_counter)
+
+        # set titles and info for random encounters
+        RE_title = QLabel("Random Encounters")
+        RE_label = QLabel("Counter")
+        self.RE_chance_lb = QLabel("Chance for at least one shiny:")
+        self.RE_chance_amount = QTextEdit("{:.2f}".format(self.RE_chance))
+
+        self.inc_btn_RE = QPushButton("+")
+        self.inc_btn_RE.clicked.connect(self.increase_RE_counter)
 
         grid = QGridLayout()
         grid.setSpacing(5)
@@ -53,11 +64,11 @@ class TestGUI(QWidget):
         grid.addWidget(self.fishing_title, 0, 0)
         grid.addWidget(self.counter_label, 1, 0)
         grid.addWidget(self.counter_disp, 1, 1)
-        grid.addWidget(self.inc_btn, 1, 2)
-        grid.addWidget(self.chance_lb, 2, 1)
-        grid.addWidget(self.chance_amount, 2, 2)
+        grid.addWidget(self.inc_btn_RE, 1, 2)
+        grid.addWidget(self.fishing_chance_lb, 2, 1)
+        grid.addWidget(self.fishing_chance_amount, 2, 2)
 
-        self.update_chance()
+        self.update_fishing_chance()
 
         self.setLayout(grid)
         self.show()
@@ -65,16 +76,26 @@ class TestGUI(QWidget):
     def increase_fishing_counter(self):
         self.fishing_counter += 1
         self.counter_disp.setText(str(self.fishing_counter))
-        self.update_chance()
+        self.update_fishing_chance()
 
         # should also then save this counter value to a json file
 
-    def update_chance(self):
+    def increase_RE_counter(self):
+        self.RE_counter += 1
+        self.counter_disp.setText(str(self.RE_counter))
+        self.update_RE_chance()
+
+        # should also then save this counter value to a json file
+
+    def update_fishing_chance(self):
 
         chance = ch.fishing(self.fishing_counter)
-        self.chance_amount.setText("{:.2f}%".format(chance * 100))
+        self.fishing_chance_amount.setText("{:.2f}%".format(chance * 100))
 
-        #QApplication.processEvents()  # update gui for pyqt
+    def update_RE_chance(self):
+
+        chance = ch.random_encounter(self.RE_counter)
+        self.RE_chance_amount.setText("{:.2f}%".format(chance * 100))
 
     def set_chain(self):
         entry = self.counter_disp.toPlainText()
@@ -84,7 +105,7 @@ class TestGUI(QWidget):
             entry = 0
 
         self.fishing_counter = entry
-        self.update_chance()
+        self.update_fishing_chance()
 
 
 if __name__ == '__main__':
