af43b9e4c5883841f071d7aa47de2be855bb9cba
JDGale8 <jake@jakegale.com>
2017-01-04 14:05:23 -0800

    contains cumulative calculations within a tab

--- bin/app.py
+++ bin/app.py
@@ -11,6 +11,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QTabWidget
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QCheckBox
@@ -29,19 +30,42 @@ class ShinyGUI(QWidget):
         self.setWindowTitle('Shiny Pokemon Simulator')
         self.setWindowIcon(QIcon('res/pokeball_favicon.png'))
 
+        # lets try creating a tab
+
+        QTab = QTabWidget()
+
+
+
         # set layouts
         # main layout will be vertical, with sub horizontal layouts within
 
-        vbox = QVBoxLayout()
-        self.setLayout(vbox)
+
+
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QTab)
+        self.setLayout(main_layout)
+
+        cum_tab = self.generate_cumulative_tab()
+        QTab.addTab(cum_tab, "Cumulative Chances")
+
+        self.update()
+
+        self.show()
+
+    def generate_cumulative_tab(self):
+
+        cum_tab = QWidget()
+        vbox = QVBoxLayout(cum_tab)
 
         checkbox_hbox = QHBoxLayout()
+
+        # CHECKBOXES #
+
         shiny_charm_chk = QCheckBox("Shiny Charm", self)
         masuda_chk = QCheckBox("Masuda Method", self)
-
         shiny_charm_chk.stateChanged.connect(self.update_shiny_charm)
         masuda_chk.stateChanged.connect(self.update_masuda)
-
         checkbox_hbox.addWidget(shiny_charm_chk)
         checkbox_hbox.addWidget(masuda_chk)
 
@@ -49,12 +73,7 @@ class ShinyGUI(QWidget):
         calculation_hbox_2 = QHBoxLayout()  # contains egg and horde
         calculation_hbox_3 = QHBoxLayout()  # contains SOS and DexNav
 
-        vbox.addLayout(checkbox_hbox)
-        vbox.addLayout(calculation_hbox_1)
-        vbox.addLayout(calculation_hbox_2)
-        vbox.addLayout(calculation_hbox_3)
-
-        # create the grid layouts
+        # CUMULATIVE GRIDS #
         self.fish_grid = gen_ui.CumGrid('fish')
         self.RE_grid = gen_ui.CumGrid('RE')
         self.horde_grid = gen_ui.CumGrid('horde')
@@ -69,9 +88,12 @@ class ShinyGUI(QWidget):
         calculation_hbox_3.addLayout(self.SOS_grid.grid)
         calculation_hbox_3.addLayout(self.dex_grid.grid)
 
-        self.update()
+        vbox.addLayout(checkbox_hbox)
+        vbox.addLayout(calculation_hbox_1)
+        vbox.addLayout(calculation_hbox_2)
+        vbox.addLayout(calculation_hbox_3)
 
-        self.show()
+        return cum_tab
 
     def update_shiny_charm(self, sc_chk):
 
